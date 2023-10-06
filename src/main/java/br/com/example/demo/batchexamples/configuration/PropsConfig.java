2c2d65af94082dc30654be2afad4884b92d79c58
lmvb <luizmig52@gmail.com>
2023-10-06 15:45:05 -0300

    feat: ADD externalizando configurações de acessa ao banco de dados

---
+++ src/main/java/br/com/example/demo/batchexamples/configuration/PropsConfig.java
@@ -0,0 +1,18 @@
+package br.com.example.demo.batchexamples.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+
+   @Bean
+    public PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        var configurer = new PropertySourcesPlaceholderConfigurer();
+        configurer.setLocation(new FileSystemResource("/etc/config/batch-examples/application.properties"));
+        return configurer;
+    }
+
+}
